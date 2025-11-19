b116671f8cde68bdb6a0304a0e923f4b8cc35c1b
patrick <patrick@noreply.local>
2025-11-19 23:37:59 +0100

    feat: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,27 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { nodePolyfills as polyfills } from "vite-plugin-node-polyfills";
 import packageJson from "./package.json";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	build: {
-		lib: {
-			name: packageJson.name,
-			entry: resolve(__dirname, "src/index.ts"),
-		},
-	},
+	plugins: [
+		polyfills({ include: ["util"], protocolImports: true }),
+		dts({ tsconfigPath: "./tsconfig.build.json", rollupTypes: true }),
+	],
 	resolve: {
 		alias: {
 			"~": resolve(__dirname, "src"),
 		},
 	},
-	plugins: [
-		nodePolyfills({ include: ["util"], protocolImports: true }),
-		dts({ exclude: ["**/*.test.ts"] }),
-	],
+	build: {
+		lib: {
+			name: packageJson.name,
+			entry: resolve(__dirname, "src/index.ts"),
+			fileName: "index",
+			formats: ["es", "cjs"],
+		},
+	},
 });
